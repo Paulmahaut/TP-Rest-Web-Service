c43a4e27a81d27e3f066831c4522465e2ce5b962
Paulmahaut <141720636+Paulmahaut@users.noreply.github.com>
2026-08-26 15:08:43 +0200

    Successful build
    The build works and you can see Hello at the local host after launch of RentingRest.java

--- src/main/java/com/example/Lab1/RentingRest.java
+++ src/main/java/com/example/Lab1/RentingRest.java
@@ -1,7 +1,18 @@
+package com.example.Lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
-public class HelloService {
-@GetMapping("/")
-public String hello() {
-return "hello";
-}
+public class RentingRest {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab1Application.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "hello";
+	}
 }
