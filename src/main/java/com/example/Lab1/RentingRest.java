404135ff9cfa684a04a79de7df28e6b8c4ab98ce
Paulmahaut <141720636+Paulmahaut@users.noreply.github.com>
2026-08-26 16:45:57 +0200

    • Get the features of a car

--- src/main/java/com/example/Lab1/RentingRest.java
+++ src/main/java/com/example/Lab1/RentingRest.java
@@ -1,18 +1,34 @@
 package com.example.Lab1;
-
 import org.springframework.boot.SpringApplication;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class RentingRest {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Lab1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1Application.class, args);
+    }
 
-	@GetMapping("/")
-	public String hello() {
-		return "hello";
-	}
+    @GetMapping("/cars/{plateNumber}")
+    public Car Carplate(@PathVariable("plateNumber") String plateNumber) {
+
+        if (plateNumber.equals("11AA22")) {
+            return new Car("11AA22", "Ferrari", 100);
+        }
+
+        throw new ResponseStatusException(
+            HttpStatus.NOT_FOUND,
+            "Car not found"
+        );
+    }
+
+    public record Car(
+        String plateNumber,
+        String brand,
+        int price
+    ) {}
 }
