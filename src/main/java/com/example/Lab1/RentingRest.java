22cae66564c4d1caeea50f5f9b252b1154dc1cfe
Paulmahaut <141720636+Paulmahaut@users.noreply.github.com>
2026-08-27 10:30:47 +0200

    Rent a car
    Non 100% functionnal

--- src/main/java/com/example/Lab1/RentingRest.java
+++ src/main/java/com/example/Lab1/RentingRest.java
@@ -1,8 +1,11 @@
 package com.example.Lab1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -26,9 +29,35 @@ public class RentingRest {
         );
     }
 
+    @PutMapping("/cars/{plateNumber}")
+    public void rent(
+        @PathVariable("plateNumber") String plateNumber,
+        @RequestParam("rent") boolean rent,
+        @RequestBody Dates dates
+    ) {
+        if (!plateNumber.equals("11AA22")) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND,
+                "Car not found"
+            );
+        }
+
+        if (!rent) {
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST,
+                "The rent parameter must be true"
+            );
+        }
+    }
+
     public record Car(
         String plateNumber,
         String brand,
         int price
+    ) {}
+
+    public record Dates(
+        String begin,
+        String end
     ) {}
 }
